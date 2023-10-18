9c23bc10ffc1d0266c96cc06bc564379232a575d
Ewerton <3w3rt0n@gmail.com>
2023-10-18 20:54:20 -0300

    Marshaller

--- Middleware/src/middleware/basic_remoting/Marshaller.java
+++ Middleware/src/middleware/basic_remoting/Marshaller.java
@@ -1,4 +1,29 @@
 package middleware.basic_remoting;
 
 public class Marshaller {
+    public static byte[] marshal(Serializable object) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+
+            objectOutputStream.writeObject(object);
+
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	public static Object unmarshal(byte[] data) {
+        try {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+            return objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
